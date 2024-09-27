d31a1fd339b7249c718d9900bf6f9fc286cc3917
No-face-117 <sayedmahamudalif@gmail.com>
2024-09-27 12:40:09 +0600

    Add files via upload

---
+++ M_F_R_L_Task_M_1ABCD.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+class M_F_R_L_Task_M_1ABCD {
+  public static void main(String [] args){
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter your number");  
+    int num = scan.nextInt(); 
+
+    evenChecker(num);
+
+    if (isEven(num) == true) {
+      System.out.println();
+      System.out.println("true");
+    }
+    else if (isEven(num) == false) {
+      System.out.println();
+      System.out.println("false");
+    }
+    else;
+
+    if (isPos(num) == true) {
+      System.out.println();
+      System.out.println("true");
+    }
+    else if (isPos(num) == false) {
+      System.out.println();
+      System.out.println("false");
+    }
+    else;
+
+    sequence(num);
+      
+  }
+
+  public static void evenChecker(int ak){
+    boolean e_v_o ;
+
+    if (ak % 2 == 0) {
+      System.out.println();
+      System.out.println("Even!!");
+    }
+    else if (ak % 2 != 0) {
+      System.out.println();
+      System.out.println("Odd!!");
+    }
+    else{}
+  }
+
+  public static boolean isEven(int ak){
+    boolean e_v_o = true;
+
+    if (ak % 2 == 0) {
+      e_v_o = true;
+    }
+    else if (ak % 2 != 0) {
+      e_v_o = false;
+    }
+    else{}
+    
+    return e_v_o;
+  }
+
+  public static boolean isPos(int ak){
+    boolean e_v_o = true;
+
+    if (ak > 0) {
+      e_v_o = true;
+    }
+    else if (ak < 0) {
+      e_v_o = false;
+    }
+    else{}
+    
+    return e_v_o;
+  }
+
+  public static void sequence(int ak){
+
+    System.out.println();
+    if (ak > 0) {
+        for (int i = 0; i <= ak; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " , ");    
+            }
+            else{}
+            
+        }
+    }
+    else if (ak < 0) {
+        for (int i = 0; ak <= i; ak++) {
+            if (ak % 2 != 0) {
+                System.out.print(ak + " , ");
+            }
+            else{}
+        }
+    }
+    else{}
+  }
+}  
+  
